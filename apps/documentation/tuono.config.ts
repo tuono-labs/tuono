a89044a78251cabc03d8dedf6941dc9574e451c5
Marco Pasqualetti <marco.pasqualetti@live.com>
2024-11-26 07:57:55 +0100

    test config inside docs

---
+++ apps/documentation/tuono.config.ts
@@ -0,0 +1,11 @@
+import type { TuonoConfig } from 'tuono/config'
+
+const config: TuonoConfig = {
+  vite: {
+    alias: {
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
+}
+
+export default config
