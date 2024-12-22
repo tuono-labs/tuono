6e2990e6bfe91f30a55e4456f1f5bd8833cfa3d2
Marco Pasqualetti <24919330+marcalexiei@users.noreply.github.com>
2024-12-22 21:15:35 +0100

    refactor: reformat exports of packages/tuono/src/ssr/index.ts

--- packages/tuono/src/ssr/index.ts
+++ packages/tuono/src/ssr/index.ts
@@ -1,4 +1,5 @@
+export { serverSideRendering } from './server'
+
 export { MessageChannelPolyfill } from './polyfills/MessageChannel'
 export { EventPolyfill } from './polyfills/Event'
 export { MessageEventPolyfill } from './polyfills/MessageEvent'
-export { serverSideRendering } from './server'
