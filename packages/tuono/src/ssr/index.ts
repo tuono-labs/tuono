f14dfb9e32178700e7fecc02d119a07a67a353ff
Valerio Ageno <valerioageno@yahoo.it>
2024-12-22 18:58:21 +0100

    feat: add Event and MessageEvent polyfills

--- packages/tuono/src/ssr/index.ts
+++ packages/tuono/src/ssr/index.ts
@@ -1,2 +1,4 @@
 export { MessageChannelPolyfill } from './polyfills/MessageChannel'
+export { EventPolyfill } from './polyfills/Event'
+export { MessageEventPolyfill } from './polyfills/MessageEvent'
 export { serverSideRendering } from './server'
---
+++ packages/tuono/src/ssr/polyfills/MessageEvent.ts
@@ -0,0 +1,24 @@
+import { EventPolyfill } from './Event'
+
+/**
+ * https://html.spec.whatwg.org/multipage/comms.html#the-messageevent-interface
+ */
+export class MessageEventPolyfill<T>
+  extends EventPolyfill
+  implements MessageEvent
+{
+  data?: T
+  lastEventId: string
+  origin: string
+  ports: ReadonlyArray<MessagePort>
+  source: MessageEventSource | null
+
+  constructor(type: string, options: MessageEventInit<T>) {
+    super(type, options)
+    this.data = options.data
+    this.lastEventId = options.lastEventId ?? ''
+    this.origin = options.origin ?? ''
+    this.ports = options.ports ?? []
+    this.source = options.source ?? null
+  }
+}
