2f25c75e89416911a54cda09ab2bc50e84c4f31d
Valerio Ageno <valerioageno@yahoo.it>
2025-01-14 19:20:53 +0100

    fix: formatting

--- packages/tuono-fs-router-vite-plugin/src/generator.ts
+++ packages/tuono-fs-router-vite-plugin/src/generator.ts
@@ -167,16 +167,17 @@ export async function routeGenerator(config = defaultConfig): Promise<void> {
   const imports = [
     ...sortedRouteNodes.map((node) => {
       const extension = node.filePath.endsWith('mdx') ? '.mdx' : ''
-      return `const ${node.variableName as string
-        }Import = ${DYNAMIC_FN}(() => import('./${replaceBackslash(
-          removeExt(
-            path.relative(
-              path.dirname(config.generatedRouteTree),
-              path.resolve(config.folderName, node.filePath),
-            ),
-            false,
+      return `const ${
+        node.variableName as string
+      }Import = ${DYNAMIC_FN}(() => import('./${replaceBackslash(
+        removeExt(
+          path.relative(
+            path.dirname(config.generatedRouteTree),
+            path.resolve(config.folderName, node.filePath),
           ),
-        )}${extension}'))`
+          false,
+        ),
+      )}${extension}'))`
     }),
   ].join('\n')
 
