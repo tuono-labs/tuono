1ff9b592b6149e790d8e53c4335a1e66f9889e2b
Valerio Ageno <valerioageno@yahoo.it>
2025-01-14 19:14:17 +0100

    chore: remove tuono-lazy-fn-vite-plugin

--- packages/tuono-lazy-fn-vite-plugin/tests/sources/dynamic-only/server.expected.tsx
+++
@@ -1,8 +0,0 @@
-import React, { Suspense, type JSX } from "react";
-import DynamicComponent from "../components/DynamicComponent";
-const Loading = (): JSX.Element => <>Loading</>;
-export default function IndexPage(): JSX.Element {
-  return <Suspense fallback={<Loading />}>
-			<DynamicComponent />
-		</Suspense>;
-}
