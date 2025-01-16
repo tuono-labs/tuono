e8eb4d0e8e14de7285676b956fd0680f30e07136
Jacob Marshall <me@jhqcat.com>
2025-01-16 19:13:00 +0000

    fix tables

--- apps/documentation/src/components/MdxProvider/MdxTable/MdxTable.tsx
+++ apps/documentation/src/components/MdxProvider/MdxTable/MdxTable.tsx
@@ -2,6 +2,8 @@ import type { DetailedHTMLProps, TableHTMLAttributes } from 'react'
 import { Table } from '@mantine/core'
 import type React from 'react'
 
+import styles from './MdxTable.module.css'
+
 type MdxTableProps = DetailedHTMLProps<
   TableHTMLAttributes<HTMLTableElement>,
   HTMLTableElement
@@ -10,15 +12,24 @@ type MdxTableProps = DetailedHTMLProps<
 function MdxTable(props: MdxTableProps): React.JSX.Element {
   const { children, ...rest } = props
   return (
-    <Table highlightOnHover stickyHeader stickyHeaderOffset={60} {...rest}>
-      {children}
-    </Table>
+    <div className={styles.tableWrapper}>
+      <Table className={styles.table} highlightOnHover {...rest}>
+        {children}
+      </Table>
+    </div>
   )
 }
 
 MdxTable.Thead = Table.Thead
 MdxTable.Tbody = Table.Tbody
-MdxTable.Tr = Table.Tr
+MdxTable.Tr = ({
+  children,
+  ...rest
+}: React.ComponentProps<typeof Table.Tr>) => (
+  <Table.Tr className={styles.tableRow} {...rest}>
+    {children}
+  </Table.Tr>
+)
 MdxTable.Th = Table.Th
 MdxTable.Td = Table.Td
 MdxTable.Caption = Table.Caption
