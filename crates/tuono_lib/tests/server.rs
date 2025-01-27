bf9d6dec6a5f855aac51e56fee133fe57a53d343
Valerio Ageno <valerioageno@yahoo.it>
2025-01-27 21:27:43 +0100

    put all the scenarion in the same test

--- crates/tuono_lib/tests/server.rs
+++ crates/tuono_lib/tests/server.rs
@@ -1,26 +1,16 @@
-use serial_test::serial;
 mod utils;
 use crate::utils::utils::MockTuonoServer;
-use std::sync::Once;
 
-static mut SERVER: Option<MockTuonoServer> = None;
-static INIT: Once = Once::new();
-
-fn get_server() -> &'static MockTuonoServer {
-    unsafe {
-        INIT.call_once(|| {
-            SERVER = Some(futures::executor::block_on(MockTuonoServer::spawn()));
-        });
-
-        SERVER.as_ref().unwrap()
-    }
-}
-
+/// Why a single test for all the endpoints?
+///
+/// Rust tests run all on the same process. No more than a single V8 instance
+/// can run on the same process.
+///
+/// TODO: Find a way to better manage the integration tests for tuono_lib.
 #[tokio::test]
-#[serial]
-async fn health_check_works() {
-    println!("Running health_check_works");
-    let app = get_server();
+async fn api_endpoint_work() {
+    let app = MockTuonoServer::spawn().await;
+
     let client = reqwest::Client::builder()
         .redirect(reqwest::redirect::Policy::none())
         .build()
@@ -37,17 +27,6 @@ async fn health_check_works() {
     // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-#[tokio::test]
-#[serial]
-async fn return_not_found_html() {
-    println!("Running return_not_found_html");
-    let app = get_server();
-
-    let client = reqwest::Client::builder().build().unwrap();
-
-    let server_url = format!("http://{}:{}", &app.address, &app.port);
 
     let response = client
         .get(&format!("{server_url}/not-found"))
@@ -55,34 +34,10 @@ async fn return_not_found_html() {
         .await
         .expect("Failed to execute request.");
 
-    // TODO: This should return a 404
+    // TODO: This should return a 404 status code
     assert!(response.status().is_success());
     assert_eq!(
         response.text().await.unwrap(),
         "<h1>404 Not found</h1><a href=\"/\">Return home</a>"
     );
-}
-
-#[tokio::test]
-#[serial]
-async fn return_route_html() {
-    println!("Running return_route_html");
-    let app = get_server();
-
-    let client = reqwest::Client::builder().build().unwrap();
-
-    let server_url = format!("http://{}:{}", &app.address, &app.port);
-
-    let response = client
-        .get(&format!("{server_url}/"))
-        .send()
-        .await
-        .expect("Failed to execute request.");
-
-    assert!(response.status().is_success());
-    assert!(response
-        .text()
-        .await
-        .unwrap()
-        .starts_with("<!DOCTYPE html>"));
 }
