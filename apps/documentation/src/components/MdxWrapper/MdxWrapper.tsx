ad2d1b2b1c62dec7d534e87d96b9e8a2f6cdf4cf
Jacob Marshall <me@jhqcat.com>
2025-01-29 15:21:19 +0000

    fix overflow issues

--- apps/documentation/src/components/MdxWrapper/MdxWrapper.tsx
+++ apps/documentation/src/components/MdxWrapper/MdxWrapper.tsx
@@ -6,20 +6,27 @@ import TableOfContents from '@/components/TableOfContents'
 import EditPage from '../EditPage'
 import MdxProvider from '../MdxProvider'
 
+import classes from './MdxWrapper.module.css'
+
 interface MdxWrapperProps {
   children: ReactNode
 }
 
 export function MdxWrapper({ children }: MdxWrapperProps): JSX.Element {
   return (
-    <Container size={1000} w="100%" display="flex" style={{ gap: 12 }}>
+    <Container
+      size={1000}
+      w="100%"
+      display="flex"
+      style={{ gap: 12, justifyContent: 'space-between' }}
+    >
       <Box
         id="mdx-root"
         component="article"
         mt="xl"
         px={16}
         py={36}
-        style={{ maxWidth: '100%', wordWrap: 'break-word' }}
+        className={classes.wrapper}
       >
         <MdxProvider>{children}</MdxProvider>
         <EditPage />
