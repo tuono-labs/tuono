ad0b09c3df926b636177647e05c41e7cf7ee55ad
Jacob Marshall <me@jhqcat.com>
2025-01-29 17:42:53 +0000

    fix(docs): fix all line and link overflow issues (#426)

--- apps/documentation/src/components/MdxProvider/MdxLink/MdxLink.tsx
+++ apps/documentation/src/components/MdxProvider/MdxLink/MdxLink.tsx
@@ -1,34 +1,44 @@
-import type { JSX, AnchorHTMLAttributes } from 'react'
-import { Button } from '@mantine/core'
+import type { JSX, AnchorHTMLAttributes, ReactNode } from 'react'
+import { Anchor } from '@mantine/core'
 import { Link } from 'tuono'
 import { IconExternalLink } from '@tabler/icons-react'
 
-export default function MdxLink(
-  props: AnchorHTMLAttributes<HTMLAnchorElement>,
-): JSX.Element {
+interface MdxLinkProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  children: ReactNode
+}
+
+export default function MdxLink(props: MdxLinkProps): JSX.Element {
   if (props.href?.startsWith('http') || props.href?.startsWith('mailto')) {
     return (
-      <Button
-        component="a"
+      <Anchor
         {...props}
         target="_blank"
-        rightSection={
-          <IconExternalLink size="16px" style={{ marginLeft: -5 }} />
-        }
         variant="transparent"
-        style={{ height: '20px' }}
+        display="inline"
+        style={{ fontWeight: 600 }}
         mt={-2}
         p={0}
-      />
+      >
+        {props.children}
+        <IconExternalLink
+          size="16px"
+          style={{
+            marginLeft: '4px',
+            display: 'inline-block',
+            transform: 'translateY(2px)',
+          }}
+        />
+      </Anchor>
     )
   }
   return (
-    <Button
+    <Anchor
       component={Link}
       {...props}
       target="_blank"
       variant="transparent"
-      style={{ height: '20px' }}
+      display="inline"
+      style={{ fontWeight: 600 }}
       mt={-2}
       p={0}
     />
