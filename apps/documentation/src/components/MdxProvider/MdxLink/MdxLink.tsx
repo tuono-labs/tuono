cb6a66b089c07188318abe860dafca5680bce417
Jacob Marshall <me@jhqcat.com>
2025-01-29 17:29:29 +0000

    implement suggestions

--- apps/documentation/src/components/MdxProvider/MdxLink/MdxLink.tsx
+++ apps/documentation/src/components/MdxProvider/MdxLink/MdxLink.tsx
@@ -11,7 +11,6 @@ export default function MdxLink(props: MdxLinkProps): JSX.Element {
   if (props.href?.startsWith('http') || props.href?.startsWith('mailto')) {
     return (
       <Anchor
-        component="a"
         {...props}
         target="_blank"
         variant="transparent"
@@ -21,9 +20,14 @@ export default function MdxLink(props: MdxLinkProps): JSX.Element {
         p={0}
       >
         {props.children}
-        <div style={{ display: 'inline-block', transform: 'translateY(3px)' }}>
-          <IconExternalLink size="16px" style={{ marginLeft: '4px' }} />
-        </div>
+        <IconExternalLink
+          size="16px"
+          style={{
+            marginLeft: '4px',
+            display: 'inline-block',
+            transform: 'translateY(2px)',
+          }}
+        />
       </Anchor>
     )
   }
