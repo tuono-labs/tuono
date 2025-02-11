0b25de6508501cd7b0fd961f468eaeec37c8b46c
Jovan Milosevic <jovan.milosevic810@gmail.com>
2025-02-11 20:59:32 +0100

    Fix e2e setup script cwd when running cargo build

--- e2e/e2e-test-setup.js
+++ e2e/e2e-test-setup.js
@@ -1,4 +1,8 @@
 import { execSync } from 'child_process'
+import path from 'path'
 
-execSync('cargo build --config opt-level=0', { stdio: 'inherit' })
+const __dirname = import.meta.dirname;
+const rootDir = path.join(__dirname, '..');
+
+execSync('cargo build --config opt-level=0', { cwd: rootDir, stdio: 'inherit' })
 execSync('turbo build', { stdio: 'inherit' })
