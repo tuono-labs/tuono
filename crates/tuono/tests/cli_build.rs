21ee168150578c7be985270cc002ae44b271a925
Jacob Marshall <me@jhqcat.com>
2025-02-12 17:49:16 +0000

    test base case

--- crates/tuono/tests/cli_build.rs
+++ crates/tuono/tests/cli_build.rs
@@ -203,6 +203,24 @@ fn build_fails_with_no_config() {
 
 #[test]
 #[serial]
+fn it_inits_new_git_repo_by_default_with_git_installed() {
+    let temp_tuono_project = TempTuonoProject::new();
+
+    std::env::set_current_dir(temp_tuono_project.path()).unwrap();
+
+    let mut test_tuono_new = Command::cargo_bin("tuono").unwrap();
+    test_tuono_new
+        .arg("new")
+        .arg(".")
+        .assert()
+        .success();
+
+    // Ensure the `.git` directory exists
+    assert!(temp_tuono_project.path().join(".git").exists());
+}
+
+#[test]
+#[serial]
 fn it_does_not_init_new_git_repo_with_git_false() {
     let temp_tuono_project = TempTuonoProject::new();
 
