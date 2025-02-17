8e04abbe425357921e6f560e18f5f724d6e13351
Valerio Ageno <51341197+Valerioageno@users.noreply.github.com>
2025-02-17 20:49:51 +0100

    feat: setup default tracing subscriber (#564)

--- crates/tuono/src/build.rs
+++ crates/tuono/src/build.rs
@@ -3,24 +3,30 @@ use spinners::{Spinner, Spinners};
 use std::path::PathBuf;
 use std::thread::sleep;
 use std::time::Duration;
+use tracing::{error, trace};
 
 use crate::app::App;
 use crate::mode::Mode;
 
+fn exit_gracefully_with_error(msg: &str) -> ! {
+    error!(msg);
+    std::process::exit(1);
+}
+
 pub fn build(mut app: App, ssg: bool, no_js_emit: bool) {
     if no_js_emit {
         println!("Rust build successfully finished");
-        return;
+        std::process::exit(0);
     }
 
     if ssg && app.has_dynamic_routes() {
         // TODO: allow dynamic routes static generation
         println!("Cannot statically build dynamic routes");
-        return;
+        std::process::exit(1);
     }
 
     app.build_tuono_config()
-        .expect("Failed to build tuono.config.ts");
+        .unwrap_or_else(|_| exit_gracefully_with_error("Failed to build tuono.config.ts"));
 
     let mut app_build_spinner = Spinner::new(Spinners::Dots, "Building app...".into());
 
@@ -35,44 +41,66 @@ pub fn build(mut app: App, ssg: bool, no_js_emit: bool) {
         let mut app_build_static_spinner =
             Spinner::new(Spinners::Dots, "Static site generation".into());
 
+        let mut exit_gracefully_with_error = |msg: &str| -> ! {
+            app_build_static_spinner.stop_with_message("\u{274C} Build failed\n".into());
+            exit_gracefully_with_error(msg)
+        };
+
         let static_dir = PathBuf::from("out/static");
 
         if static_dir.is_dir() {
-            std::fs::remove_dir_all(&static_dir).expect("Failed to clear the out/static folder");
+            std::fs::remove_dir_all(&static_dir).unwrap_or_else(|_| {
+                exit_gracefully_with_error("Failed to clear the out/static folder")
+            });
         }
 
-        std::fs::create_dir(&static_dir).expect("Failed to create static output dir");
+        std::fs::create_dir(&static_dir)
+            .unwrap_or_else(|_| exit_gracefully_with_error("Failed to create static output dir"));
 
         copy(
             "./out/client",
             static_dir,
             &CopyOptions::new().overwrite(true).content_only(true),
         )
-        .expect("Failed to clone assets into static output folder");
+        .unwrap_or_else(|_| {
+            exit_gracefully_with_error("Failed to clone assets into static output folder")
+        });
 
         // Start the server
         #[allow(clippy::zombie_processes)]
         let mut rust_server = app.run_rust_server();
 
+        let mut exit_and_shut_server = |msg: &str| -> ! {
+            _ = rust_server.kill();
+            exit_gracefully_with_error(msg)
+        };
+
         let reqwest_client = reqwest::blocking::Client::builder()
             .user_agent("")
             .build()
-            .expect("Failed to build reqwest client");
+            .unwrap_or_else(|_| exit_and_shut_server("Failed to build reqwest client"));
 
         // Wait for server
         let mut is_server_ready = false;
         let config = app.config.as_ref().unwrap();
 
         while !is_server_ready {
+            trace!("Checking server availability");
             let server_url = format!("http://{}:{}", config.server.host, config.server.port);
             if reqwest_client.get(&server_url).send().is_ok() {
                 is_server_ready = true;
+            } else {
+                trace!("Server not ready yet. Sleeping for 1 second");
             }
             sleep(Duration::from_secs(1));
         }
 
+        trace!("Server is ready, starting static site generation");
+
         for (_, route) in app.route_map {
-            route.save_ssg_file(&reqwest_client);
+            if let Err(msg) = route.save_ssg_file(&reqwest_client) {
+                exit_and_shut_server(&msg);
+            }
         }
 
         // Close server
