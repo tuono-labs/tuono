8eceb32f36da62d98daac365a9e30fbb28c54ffb
Valerio Ageno <valerioageno@yahoo.it>
2025-03-09 13:11:40 +0100

    test-services-folder

--- crates/tuono_lib/src/lib.rs
+++ crates/tuono_lib/src/lib.rs
@@ -6,13 +6,13 @@
 mod catch_all;
 mod config;
 mod env;
-mod logger;
 mod manifest;
 mod mode;
 mod payload;
 mod request;
 mod response;
 mod server;
+mod services;
 mod ssr;
 mod vite_reverse_proxy;
 mod vite_websocket_proxy;
