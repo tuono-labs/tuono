8c3f45d273de21ae44bd2b2d578515788824648c
Valerio Ageno <51341197+Valerioageno@users.noreply.github.com>
2025-04-10 22:08:28 +0200

    feat: prevent file generation if type not changed (#735)

--- crates/tuono/src/typescript/types_jar.rs
+++ crates/tuono/src/typescript/types_jar.rs
@@ -4,18 +4,23 @@ use std::collections::HashMap;
 use std::env;
 use std::fs::read_to_string;
 use std::path::{Path, PathBuf};
-use tracing::error;
+use tracing::{error, trace};
+use tuono_internal::tuono_println;
 
 const TUONO_MACRO_TRAIT_NAME: &str = "Type";
 
 #[derive(Debug, Clone, Default)]
 pub struct TypesJar {
     types: Vec<FileTypes>,
+    should_generate_typescript_file: bool,
 }
 
 impl TypesJar {
     pub fn new() -> Self {
-        Self { types: vec![] }
+        Self {
+            types: vec![],
+            should_generate_typescript_file: true,
+        }
     }
 }
 
@@ -24,6 +29,7 @@ impl TypesJar {
     /// present in the provided `file_path`.
     /// This function is triggered when a file is deleted
     pub fn remove_file(&mut self, file_path: PathBuf) {
+        self.should_generate_typescript_file = true;
         self.types.retain(|ttype| ttype.file_path != file_path);
     }
 
@@ -31,11 +37,24 @@ impl TypesJar {
         if let Ok(file_str) = read_to_string(&path) {
             if file_str.contains(TUONO_MACRO_TRAIT_NAME) {
                 if let Ok(ttype) = FileTypes::try_from((path.clone(), file_str)) {
-                    self.types.retain(|t| t.file_path != path);
+                    if Some(&ttype) == self.types.iter().find(|t| t.file_path == path) {
+                        // The new file exactly matches the old one
+                        trace!("File already exists in jar: {:?}", path);
+                        return;
+                    }
+                    trace!("Refreshing: {:?} type", ttype.types);
+
+                    self.should_generate_typescript_file = true;
+                    self.remove_file(path);
                     self.types.push(ttype);
                 } else {
                     error!("Failed to parse file: {:?}", path);
                 }
+            } else {
+                // Check if the file exist. In case it is it means that the user
+                // removed the "Type" derived trait. Hence we have to remove it from
+                // the jar
+                self.remove_file(path.clone());
             }
         } else {
             error!("Failed to read file: {:?}", path);
@@ -43,6 +62,7 @@ impl TypesJar {
     }
 
     pub fn check_duplicate_types(&self) -> HashMap<String, (PathBuf, PathBuf)> {
+        trace!("Checking for duplicated types");
         let mut duplicates: HashMap<String, (PathBuf, PathBuf)> = HashMap::new();
         let mut paths: Vec<&PathBuf> = vec![];
         let mut types: Vec<&Vec<String>> = vec![];
@@ -74,19 +94,18 @@ impl TypesJar {
         let base_path_str = base_path.to_string_lossy();
 
         for (type_name, file_paths) in duplicates.iter() {
-            // TODO: replace this with tuono_println! macro
-            println!(
-                "  Duplicate \"{}\" type found in files:\n\n  - {}\n  - {}\n",
-                type_name,
-                file_paths
-                    .0
-                    .to_string_lossy()
-                    .replace(&base_path_str.to_string(), ""),
-                file_paths
-                    .1
-                    .to_string_lossy()
-                    .replace(&base_path_str.to_string(), ""),
-            );
+            let first_file_path = file_paths
+                .0
+                .to_string_lossy()
+                .replace(&base_path_str.to_string(), "");
+            let second_file_path = file_paths
+                .1
+                .to_string_lossy()
+                .replace(&base_path_str.to_string(), "");
+
+            tuono_println!("Duplicate \"{}\" type found in files:\n", type_name);
+            tuono_println!("- {}", first_file_path);
+            tuono_println!("- {}\n", second_file_path);
         }
     }
 
@@ -107,10 +126,18 @@ impl TypesJar {
         typescript
     }
 
-    pub fn generate_typescript_file(&self, base_path: &Path) -> std::io::Result<()> {
+    pub fn generate_typescript_file(&mut self, base_path: &Path) -> std::io::Result<()> {
+        if !self.should_generate_typescript_file {
+            trace!("No need to create typescript module file");
+            return Ok(());
+        }
+        self.should_generate_typescript_file = false;
+        trace!("Creating typescript module file");
         let typescript = self.generate_typescript();
         let typescript_file_path = base_path.join(".tuono").join("types.ts");
         std::fs::write(typescript_file_path, typescript)?;
+
+        trace!("Typescript module file created");
         Ok(())
     }
 }
@@ -126,12 +153,13 @@ impl From<&PathBuf> for TypesJar {
                 files.for_each(|path| {
                     let file_path = path.unwrap_or_default();
                     if let Ok(file_str) = read_to_string(&file_path) {
-                        if file_str.contains(TUONO_MACRO_TRAIT_NAME) {
-                            if let Ok(ttype) = FileTypes::try_from((file_path.clone(), file_str)) {
-                                jar.types.push(ttype);
-                            } else {
-                                error!("Failed to parse file: {:?}", file_path);
-                            }
+                        if !file_str.contains(TUONO_MACRO_TRAIT_NAME) {
+                            return;
+                        }
+                        if let Ok(ttype) = FileTypes::try_from((file_path.clone(), file_str)) {
+                            jar.types.push(ttype);
+                        } else {
+                            error!("Failed to parse file: {:?}", file_path);
                         }
                     } else {
                         error!("Failed to read file: {:?}", file_path);
@@ -208,5 +236,25 @@ mod tests {
                 .collect::<Vec<&String>>()
                 .contains(&&"Type4".to_string())
         );
+    }
+
+    #[test]
+    fn it_correctly_removes_types() {
+        let mut jar = TypesJar::new();
+        let file_path = PathBuf::from("src/types.rs");
+        let file_type = FileTypes {
+            file_path: file_path.clone(),
+            types_as_string: String::from("type1"),
+            types: vec![String::from("Type1")],
+        };
+        assert!(jar.should_generate_typescript_file);
+        //Force file generation to false
+        jar.should_generate_typescript_file = false;
+
+        jar.types.push(file_type);
+        jar.remove_file(file_path.clone());
+
+        assert!(jar.should_generate_typescript_file);
+        assert_eq!(jar.types.len(), 0);
     }
 }
